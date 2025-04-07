c065d96d7e901d4037a97482ddf4498a1ee5ff43
hollyfwei <146286100+hollyfwei@users.noreply.github.com>
2025-04-07 11:03:12 -0500

    more changes

--- src/cs220hw/urls.py
+++ src/cs220hw/urls.py
@@ -16,8 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path("rabble/", include("rabble.urls")),
     path("admin/", admin.site.urls),
 ]
