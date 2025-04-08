aedbd45c4b29700531f85ecf9c2e646e7c1fa932
holly <hfwei@HWSurface.>
2025-04-07 21:43:06 -0500

    Minor changes to views

--- src/rabble/views.py
+++ src/rabble/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect    
 from django.http import HttpResponse
+from django.contrib.auth import logout
 
 def index(request):
     context = {"welcome": "Hello, world!"}
@@ -8,7 +9,3 @@ def index(request):
 
 def profile(request):
     return render(request, "rabble/profile.html")
-
-def logout(request):
-    logout(request)
-    return redirect('index')
