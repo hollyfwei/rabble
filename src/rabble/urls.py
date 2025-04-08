8ff077ed770592277f12367546c8e6709f5bdb30
holly <hfwei@HWSurface.>
2025-04-07 21:46:08 -0500

    Final adjustments

--- src/rabble/urls.py
+++ src/rabble/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("profile", views.profile, name="profile"),
-    path("logout", views.logout, name="logout")
+    path("profile/", views.profile, name="profile")
 ]
