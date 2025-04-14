605e5f7e590fca57c02b47184762c89e6ec98b5d
holly <hfwei@HWSurface.>
2025-04-14 17:55:04 -0500

    HW2 - Added Models (no join tables)

--- src/rabble/models.py
+++ src/rabble/models.py
@@ -29,4 +29,42 @@ class Subrabble(models.Model):
 
     class Meta:
         unique_together = ('community_id', 'subrabble_name')
+
+class Post(models.Model):
+    post_id = models.IntegerField(primary_key=True)
+    subrabble_id = models.ForeignKey(Subrabble, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.TextField()
+    body = models.TextField()
+    anonymity = models.BooleanField(default=False)
+
+class Comment(models.Model):
+    comment_id = models.IntegerField(primary_key=True)
+    parent_id = models.ForeignKey('self', null=True, blank=True, on_delete=models.CASCADE)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField()
+    anonymity = models.BooleanField(default=False)
+
+class Like(models.Model):
+    class LikeableType(models.IntegerChoices):
+        POST = 1, "Post"
+        COMMENT = 2, "Comment"
+    likeable_type = models.PositiveSmallIntegerField(choices=LikeableType.choices, default=LikeableType.POST)
+    like_id = models.IntegerField(primary_key=True)
+    post_id = models.ForeignKey(Post, on_delete=models.CASCADE, null=True, blank=True)
+    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE, null=True, blank=True)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Conversation(models.Model):
+    conversation_id = models.IntegerField(primary_key=True)
+    community_id = models.ForeignKey(Community, on_delete=models.CASCADE)
+    title = models.TextField()
+
+class Message(models.Model):    
+    message_id = models.IntegerField(primary_key=True)
+    conversation_id = models.ForeignKey(Conversation, on_delete=models.CASCADE)
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE)
+    text = models.TextField()
+    timestamp = models.DateTimeField(auto_now_add=True)
     
