a4216cfc92e63f9ae35b6d8a617593981b988c1f
holly <hfwei@HWSurface.>
2025-04-17 17:45:58 -0500

    HW2 - Additional Tasks

--- src/rabble/admin.py
+++ src/rabble/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User, Community, Follow, Subrabble, Post, Comment, Like, Conversation, Message
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Community)
+admin.site.register(Follow)
+admin.site.register(Subrabble)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Conversation)
+admin.site.register(Message)
