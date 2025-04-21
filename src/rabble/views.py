718714c5da80e14c7540f0c2694c5f5280854eb0
Holly Wei <hfwei@uchicago.edu>
2025-04-21 14:37:24 -0500

    HW3 - Additional Tasks started1

--- src/rabble/views.py
+++ src/rabble/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404   
 from django.http import HttpResponse
+from django.contrib import messages
 from django.contrib.auth import logout
+from django.contrib.auth.decorators import login_required
 from .models import Subrabble, Community, Post
 from .forms import PostForm
 
@@ -9,15 +11,18 @@ def index(request):
     subrabbles = Subrabble.objects.filter(community=default_com)
     return render(request, "rabble/index.html", {"subrabbles": subrabbles})
 
+@login_required
 def profile(request):
     return render(request, "rabble/profile.html")
 
+@login_required
 def subrabble_detail(request, identifier):
     subrabble = Subrabble.objects.get(identifier=identifier)
     posts = subrabble.posts.all()
     return render(request, "rabble/subrabble_detail.html", 
                   {"subrabble": subrabble, "posts": posts})
 
+@login_required
 def post_detail(request, identifier, pk):
     subrabble = Subrabble.objects.get(identifier=identifier)
     post = subrabble.posts.get(pk=pk)
@@ -27,6 +32,7 @@ def post_detail(request, identifier, pk):
                    "post": post , 
                    "comments": comments})
 
+@login_required
 def post_create(request, identifier):
     subrabble = Subrabble.objects.get(identifier=identifier)
     if request.method == 'POST':
@@ -38,9 +44,15 @@ def post_create(request, identifier):
         form = PostForm()
     return render(request, "rabble/post_form.html", {"form": form, "subrabble": subrabble})
 
+@login_required
 def post_edit(request, identifier, pk):
     subrabble = Subrabble.objects.get(identifier=identifier)
     post = get_object_or_404(Post, pk=pk)
+
+    if post.user != request.user:
+        messages.warning(request, "You are not authorized to edit this post.")
+        return redirect("post-detail", identifier=subrabble.identifier, pk=post.pk)
+    
     if request.method == 'POST':
         form = PostForm(request.POST, instance=post)
         if form.is_valid():
@@ -48,4 +60,5 @@ def post_edit(request, identifier, pk):
             return redirect(reverse('post-detail', identifier=subrabble.identifier, pk=post.pk))
     else:
         form = PostForm(instance=post)
+
     return render(request, "rabble/post_form.html", {"form": form, "subrabble": subrabble})
