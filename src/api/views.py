e26b7cc76ba34af3103419ab5cc5c06ef7adf167
Holly Wei <hfwei@uchicago.edu>
2025-04-30 00:11:36 -0500

    HW4 - Task 1 update

--- src/api/views.py
+++ src/api/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from rest_framework import status, generics
 from rest_framework.decorators import api_view
-from rest_framework.reponse import Response
+from rest_framework.response import Response
 
 from rabble.models import *
 from .serializers import *
