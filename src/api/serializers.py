8291b3bd88290458f58464cda64f0336a5fcd950
Holly Wei <hfwei@uchicago.edu>
2025-04-30 14:30:41 -0500

    HW4 - Additional Task 3 Complete

--- src/api/serializers.py
+++ src/api/serializers.py
@@ -2,37 +2,24 @@ from rest_framework import serializers
 from rabble.models import *
 
 class SubrabbleSerializer(serializers.ModelSerializer):
-    identifier_str = serializers.CharField(source='identifier', read_only=True)
-    visibility_str = serializers.SerializerMethodField()
-    community_str = serializers.CharField(source='community.community_name', read_only=True)
-    subrabble_str = serializers.CharField(source='subrabble_name', read_only=True)
-    description_str = serializers.CharField(source='description', read_only=True)
-    anonymous_permissions_str = serializers.BooleanField(source='anonymous_permissions', read_only=True)
-    users_str = serializers.StringRelatedField(source='users', many=True, read_only=True)
+    visibility_status = serializers.SerializerMethodField()
+    community_name = serializers.CharField(source='community.community_name', read_only=True)
 
     class Meta:
         model = Subrabble
-        fields = ['identifier', 'visibility', 'community', 'subrabble_name', 
-                  'description', 'anonymous_permissions', 'users', 'identifier_str',
-                  'visibility_str', 'community_str', 'subrabble_str', 
-                  'description_str', 'anonymous_permissions_str', 'users_str']
+        fields = ['subrabble_name', 'identifier', 'community_name', 'description', 
+                  'users', 'visibility_status', 'anonymous_permissions']
         extra_kwargs = {
             'url': {'lookup_field': 'identifier'}
         }
 
-    def get_visibility_str(self, obj):
+    def get_visibility_status(self, obj):
         return obj.get_visibility_display()
 
 class PostSerializer(serializers.ModelSerializer):
-    subrabble_identifier_str = serializers.CharField(source='subrabble.identifier', read_only=True)
-    
-    subrabble_str = serializers.CharField(source='subrabble.subrabble_name', read_only=True)
-    user_str = serializers.CharField(source='user.username', read_only=True)
-    title = serializers.CharField(source='title', read_only=True)
-    body_str = serializers.CharField(source='body', read_only=True)
-    anonymity_str = serializers.BooleanField(source='anonymity', read_only=True)
+    subrabble_identifier= serializers.CharField(source='subrabble.identifier', read_only=True)
+    author = serializers.CharField(source='user.username', read_only=True)
 
     class Meta:
         model = Post
-        fields = ['subrabble_identifier_str', 'subrabble', 'user', 'title', 'body', 'anonymity',
-                  'subrabble_str', 'user_str', 'body_str', 'anonymity_str']
+        fields = ['title', 'subrabble_identifier', 'author', 'body']
