b35523ad4fcde45ee5ad6ddd999a4b2150834198
Holly Wei <hfwei@uchicago.edu>
2025-04-30 12:21:26 -0500

    HW1 - Task 1 Complete

--- src/api/views.py
+++ src/api/views.py
@@ -103,3 +103,4 @@ class PostList(generics.ListCreateAPIView):
 class PostDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    
