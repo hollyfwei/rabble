8ad05fef9914cd824590645d64e90015a32d7054
Holly Wei <hfwei@uchicago.edu>
2025-04-30 11:28:45 -0500

    HW4 - Task 1 urls doublechecked, troubleshooting cont.

--- src/api/urls.py
+++ src/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path('subrabbles/', views.SubrabbleList.as_view(), name='api-subrabble-list'),
-    path('subrabbles/<str:identifier>', views.SubrabbleDetail.as_view(), name='api-subrabble-detail'),
-    path('subrabbles/<str:identifier>/posts/', views.PostList.as_view(), name='api-post-list'),
-    path('subrabbles/<str:identifier>/posts/<int:pk>/', views.PostDetail.as_view(), name='api-post-detail')
+    path('subrabbles/!<str:identifier>', views.SubrabbleDetail.as_view(), name='api-subrabble-detail'),
+    path('subrabbles/!<str:identifier>/posts', views.PostList.as_view(), name='api-post-list'),
+    path('subrabbles/!<str:identifier>/posts/<int:pk>', views.PostDetail.as_view(), name='api-post-detail')
 ]
--- src/rabble/urls.py
+++ src/rabble/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 
 from . import views
 
@@ -8,5 +8,6 @@ urlpatterns = [
     path("!<slug:identifier>/", views.subrabble_detail, name="subrabble-detail"),
     path("!<slug:identifier>/<int:pk>/", views.post_detail, name="post-detail"),
     path("!<slug:identifier>/new", views.post_create, name="post-create"),
-    path("!<slug:identifier>/<int:pk>/edit", views.post_edit, name="post-edit")
+    path("!<slug:identifier>/<int:pk>/edit", views.post_edit, name="post-edit"),
+    path("api/", include("api.urls"))
 ]
